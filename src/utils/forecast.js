f29e3390e39288f9643c2e841a83d5b4f423abcb
Janda <kjanda@lgsinnovations.com>
2021-09-06 13:18:27 -0400

    Added humidity and UV index

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = ((latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees.'
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees and it feels like ' + body.current.feelslike + ' degrees. The humidity is ' + body.current.humidity + '% with a UV index of ' + body.current.uv_index + '.'
             )
             }
     })
